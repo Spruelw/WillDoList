f0a192eab82922ac7a345a0bd54aeb6a1f84e8c4
Spruelw <137451524+Spruelw@users.noreply.github.com>
2024-09-14 01:20:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express, { json } from "express";
 import pg from "pg";
 import 'dotenv/config';
 import bcrypt from "bcrypt"
-import "cookie-session";
+import * as cookieSession from "cookie-session";
 import passport from "passport";
 import { Strategy } from 'passport-local';
 import GoogleStrategy from "passport-google-oauth2"
@@ -26,7 +26,7 @@ db.connect();
 
 app.use(express.static("./public"))
 
-app.use(cookie-session({
+app.use(cookieSession({
     name : "session",
     secret: process.env.SESSION_SECRET
 }))
