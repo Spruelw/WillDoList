7b9e54a07dc525eddc6f49080c8aeb2a82666195
Spruelw <137451524+Spruelw@users.noreply.github.com>
2024-09-14 01:23:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express, { json } from "express";
 import pg from "pg";
 import 'dotenv/config';
 import bcrypt from "bcrypt"
-import * as cookieSession from "cookie-session";
+import cookieSession from "cookie-session";
 import passport from "passport";
 import { Strategy } from 'passport-local';
 import GoogleStrategy from "passport-google-oauth2"
